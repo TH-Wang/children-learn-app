38a438528cf5933164f22eba721e8a4a851d0bb7
MissGuoGit <2769014334@qq.com>
2020-12-19 11:22:18 +0800

    列表组件封装

---
+++ components/readList.vue
@@ -0,0 +1,53 @@
+<template>
+	<view>
+		 aaaaaaaaaa
+		 <view class="read-list">
+		 	 <view class="read-img">
+		 	 	<image src="../static/logo.png" mode=""></image>
+		 	 </view>
+			 <view class="item-title">
+			 	汉字
+			 </view>
+			 <view class="item-description">
+			 	111111111111111111111111111111
+			 </view>
+		 </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.read-list{
+	
+	.read-img{
+		width: 178px;
+		height: 230px;
+		background: #E3E3E3;
+		border-radius: 20px;
+		image{
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.item-title{
+		font-size: 24px;
+		font-family: "PingFang";
+		font-weight: 500;
+		color: #333333;
+	}
+	.item-description{
+		font-size: 24px;
+		font-family: "PingFang";
+		color: #666666;
+	}
+}
+</style>
---
+++ pages/listen/listen.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<list-item v-for='(item,index) in commonList' :listData="item||{}" :key='index'></list-item>
+	</view>
+</template>
+
+<script>
+	import {commonList} from '../index/data';
+	import commonListItem from '../../components/commonList.vue';
+	export default {
+		data() {
+			return {
+				commonList: commonList,
+			}
+		},
+		methods: {
+			
+		},
+		components:{
+			listItem:commonListItem,
+		}
+	}
+</script>
+
+<style>
+
+</style>
